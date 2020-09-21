09dc1edd789886b98e273ad6221b7962cb9beb9c
Neelarghya Mandal <nmandal@lenovo.com>
2020-09-21 23:31:55 +0530

    [Neel] Initial Commit

---
+++ Assets/Tests/NewTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTest
+    {
+        [Test]
+        public void NewTestSimplePasses()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [UnityTest]
+        public IEnumerator NewTestWithEnumeratorPasses()
+        {
+            yield return null;
+            Assert.IsFalse(true);
+        }
+    }
+}
